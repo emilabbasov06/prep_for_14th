3c91a9b035ea72d798ef5a453470eede87260b1c
emilabbasov06 <emilabb13@gmail.com>
2025-02-10 18:34:24 +0400

    yeah it is hard to play in this life

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+const Hero = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-gray-200 mb-6">Love Calculator 3000 💖</h1>
+      <Card />
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Hero from "../components/Hero";
+
+const Home = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <Hero />
+    </div>
+  );
+};
+
+export default Home;
